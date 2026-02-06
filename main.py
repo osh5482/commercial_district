ad259e38a1c23320bb76edbc9d30cf6fecead656
osh5482 <osh5482@naver.com>
2026-02-06 21:18:38 +0900

    main.py -> json 출력 함수 추가

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from src.clients import StoreZoneClient, StoreClient, UpjongClient, DistrictClient
+import json
 
 
-def main():  # 테스트
-    district_client = DistrictClient()
-    result = district_client.get_districtList(catId="zone", parents_Cd="11680")
-    print(result)
+def print_json(json_data):
+    """JSON 데이터를 가독성 좋게 출력하는 유틸리티 함수"""
+    print(json.dumps(json_data, indent=2, ensure_ascii=False))
+
+
+def main():
+    """테스트용 메인 함수"""
+    store_zone_client = StoreZoneClient()
+    result = store_zone_client.get_storeZoneOne(10000, "2025")
+    print_json(result)
     return
 
 
